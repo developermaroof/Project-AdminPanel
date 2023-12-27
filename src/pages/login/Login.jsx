ba96cf19ab412a3adb0ac8f5686f9404a58f9111
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-12-27 13:28:06 +0500

    Updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
 import "./login.scss";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(true);
+      });
   };
 
   return (
     <div className="login">
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
         {error && <span>Wrong email or password!</span>}
       </form>
